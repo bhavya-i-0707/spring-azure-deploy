faf6f79476397237ca33a15f4df21da0d3956a8f
PoojaRaju <poojapurushatma@gmail.com>
2022-04-28 12:45:23 -0500

    first commit

---
+++ src/main/java/com/springdeploy/azuredemo/DeployOfSpringInAzureApplication.java
@@ -0,0 +1,19 @@
+package com.springdeploy.azuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DeployOfSpringInAzureApplication {
+	@GetMapping("/message")
+    public String message(){
+	return "Your is deployed successfully !!";
+    }
+	public static void main(String[] args) {
+		SpringApplication.run(DeployOfSpringInAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdeploy/azuredemo/DeployOfSpringInAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdeploy.azuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployOfSpringInAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
